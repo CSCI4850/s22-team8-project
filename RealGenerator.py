6e712d110bc2077bcfdfbf408f4a9a5475a106d8
chandlerstewart <user email>
2022-04-24 20:56:36 -0500

    moved model.h5 to project_demo folder

--- RealGenerator.py
+++ RealGenerator.py
@@ -1,28 +1,32 @@
 import numpy as np
 import csv
+import random
 from sklearn.preprocessing import MinMaxScaler
 
 class RealGenerator:
     
-    def __init__(self, recipepath, nutrpath):
-        recipefile = open(recipepath)
-        recipe_quant = list(csv.reader(recipefile))
-        recipe_quant = np.array(recipe_quant)
-        recipe_quant = np.array(recipe_quant[1:13,1:-2],dtype=float)#remove headers and scaled recipes
-        
-        
+    def __init__(self, nutrpath):
+        recipe_quant = self._build_quantity(100)
         nutrfile = open(nutrpath)
         ingr_nutr = list(csv.reader(nutrfile))
         ingr_nutr = np.array(ingr_nutr)
-        ingr_nutr = np.array(ingr_nutr[1:,1:],dtype=float)
-        ingr_nutr /= 100 #scale by 1g instead of 100g
+        
+        
+        self.ingr_nutr = np.array(ingr_nutr[1:,1:],dtype=float)
+        self.ingr_nutr /= 100 #scale by 1g instead of 100g
         
         self.scalers = {}
-        self.quant = recipe_quant
-        self.recipes = self._build_recipe_nutr(recipe_quant,ingr_nutr)
+        self.quant = None
+        self.recipes = None
+        
+    def generate(self,num):
+        self.quant = self._build_quantity(num)
+        self.recipes = self._build_recipe_nutr(self.quant,self.ingr_nutr)
         
     def _build_recipe_nutr(self,recipe_ingr,ingr_nutr):
         recipes = []
+        
+        print(recipe_ingr.shape)
         for i in range(recipe_ingr.shape[0]): # for each recipes
             recipe = recipe_ingr[i]
 
@@ -36,7 +40,67 @@ class RealGenerator:
         recipes = recipes.reshape((-1,ingr_nutr.shape[0],ingr_nutr.shape[1]))
         return recipes
 
+    
+    def _build_quantity(self, num_of_recipes):
+        
+        quants = []
+        ing_averages = [1,59.5,2.1,73.3,3.8,2.3,9.8,1.3,75,6.4,3.5,2.5,7.9,100.6,37.5]
+        ing_stdev = [0,35,9,1.0,21.8,1.0,1.3,1.4,0.3,0.5,0.5,0.5,56.7,5.4]
+        
+        for i in range(num_of_recipes):
+        
+            ingrs = np.zeros((15))
+            ingrs[0] = 1
 
+            r = random.random()
+            # Liquids
+            if r < 0.2: #1/5 chance of Milk
+                ingrs[14] = 1
+            if r >= 0.1:#9/10 chance of Water
+                ingrs[1] = 1
+
+            # Which - if any - sweetener
+            r = random.random()
+            if r < .1:
+                ingrs[11] = 1
+            elif r < .2:
+                ingrs[12] = 1
+            elif r < .8:
+                ingrs[5] = 1
+
+            # Which - if any - lipids?
+            r = random.random()    
+            if r < .25:
+                ingrs[9] = 1
+            elif r < .5:
+                ingrs[10] = 1
+            elif r < .75:
+                ingrs[4] = 1
+
+            ingrs[2] = 1 #salt
+
+            #Egg ?
+            r = random.random()    
+            if r < .2:
+                ingrs[6] = 1
+
+            #Dry Milk ?
+            r = random.random()    
+            if r < .1:
+                ingrs[7] = 1
+
+            ingrs[0] = random.random() * 12.5 + 2.5
+
+            for i in range(1, 14):
+                if (ingrs[i] > 0):
+                    ingrs[i] = np.random.normal(ing_averages[i], ing_stdev[i])
+                    if (ingrs[i]) < 0:
+                        ingrs[i] = 0
+                        
+            quants.append(ingrs)
+            
+        return np.array(quants)
+            
     def normalize(self):
         
         self.scalers['recipes'] = []
@@ -60,3 +124,25 @@ class RealGenerator:
         else:
             quant = (self.scalers['quant'].inverse_transform(quant.T)).T
             return quant
+
+        
+    def rank(self):
+        ranked = np.zeros((self.quant.shape))
+        argsort = np.argsort(self.quant)[:,::-1]
+        
+        for i in range(0, argsort.shape[0]):
+            count = self.quant.shape[1] - 1
+            ranked[i,argsort[i,0]] = count
+            
+            for j in range(1, argsort.shape[1]):
+                ind1 = argsort[i,j-1]
+                ind2 = argsort[i,j]
+
+                count -= 1
+                
+                ranked[i,ind2] = count
+
+            
+            
+        ranked = ranked.reshape((self.recipes.shape[0], self.recipes.shape[1], 1))
+        self.recipes = np.concatenate((self.recipes,ranked), axis = 2)
